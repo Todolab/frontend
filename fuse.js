fb25c4937ae08d1557e68d669720ea70c0c4003d
stevenjlho <stevenjlho@gmail.com>
2017-08-06 23:26:03 +0800

    Add develop and production task

--- fuse.js
+++ fuse.js
@@ -1,42 +1,55 @@
 const {
-  FuseBox,
-  SVGPlugin,
-  CSSPlugin,
-  BabelPlugin,
-  WebIndexPlugin,
+    FuseBox,
+    SVGPlugin,
+    CSSPlugin,
+    BabelPlugin,
+    QuantumPlugin,
+    WebIndexPlugin,
+    Sparky
 } = require("fuse-box");
 
 const path = require('path')
 
-
 let fuse, app, vendor, isProduction;
+Sparky.task("config", () => {
+    fuse = new FuseBox({
+        homeDir: "src/",
+        sourceMaps: true,
+        hash: isProduction,
+        output: "dist/$name.js",
+        plugins: [
+            SVGPlugin(), CSSPlugin(), BabelPlugin(),
+            WebIndexPlugin({
+              title: "Todolab",
+              template: path.resolve(__dirname, 'src', 'index.html')
+            }),
+            isProduction && QuantumPlugin({
+                removeExportsInterop: false,
+                uglify: true
+            })
+        ]
+    });
+    // vendor
+    vendor = fuse.bundle("vendor").instructions("~ index.jsx")
 
-
-fuse = FuseBox.init({
-  homeDir: "src",
-  output: "dist/$name.js",
-  sourceMaps: {project: true, vendor: true},
-  plugins: [
-    SVGPlugin(), CSSPlugin(), BabelPlugin(),
-    WebIndexPlugin({
-      title: "Todolab",
-      template: path.resolve(__dirname, 'src', 'index.html')
-    })
-  ]
+    // bundle app
+    app = fuse.bundle("app").instructions("> [index.jsx]")
 });
 
-// vendor
-vendor = fuse.bundle("vendor").instructions("~ index.jsx")
+Sparky.task("default", ["clean", "config"], () => {
+    fuse.dev({
+      port: process.env.PORT
+    });
+    // add dev instructions
+    app.watch().hmr()
 
-// bundle app
-app = fuse.bundle("app").instructions("> [index.jsx]")
-
-
-fuse.dev({
-  port: process.env.PORT
+    return fuse.run({chokidar: {usePolling: true}})
 });
 
-app.watch().hmr()
-
-// detect file changes on a mounted volume
-fuse.run({chokidar: {usePolling: true}})
+Sparky.task("clean", () => Sparky.src("dist/").clean("dist/"));
+Sparky.task("prod-env", ["clean"], () => { isProduction = true })
+Sparky.task("dist", ["prod-env", "config"], () => {
+    // comment out to prevent dev server from running (left for the demo)
+    fuse.dev();
+    return fuse.run();
+});
